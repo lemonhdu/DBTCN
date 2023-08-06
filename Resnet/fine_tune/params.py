a677ec44be594654b6151c1b166322d6fb51de5f
lemonhdu <813077173@qq.com>
2023-08-06 16:48:14 +0800

    Update params.py

--- Resnet/fine_tune/params.py
+++ Resnet/fine_tune/params.py
@@ -1,8 +1,5 @@
 import argparse
 
-# action_dict = {"start": 0, "tomato": 1, "cheese": 2, "lettuce": 3, "salt": 4, "vinegar": 5, "oil": 6,
-#                "pepper": 7, "dressing": 8, "end": 9, "cucumber": 10, "ingredients": 11, "salad": 12}
-
 action_dict = {"background": 0, "bread": 1, "cheese": 2, "mayonnaise": 3, "mustard": 4, "cup": 5, "coffee": 6,
                 "spoon": 7, "water": 8, "honey": 9, "sugar": 10, "hotdog": 11, "ketchup": 12, "jam": 13, "peanut": 14,
                 "chocolate": 15, "tea": 16}
